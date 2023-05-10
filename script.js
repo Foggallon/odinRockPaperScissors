5681186fd4f50ce01d5c961273f67bbdcf15f2f1
Foggallon <121718984+Foggallon@users.noreply.github.com>
2023-05-10 14:23:39 +0200

    make game function

--- script.js
+++ script.js
@@ -25,9 +25,47 @@ function playRound(playerSelection,computerSelection)
        else if(computerSelection==="Paper") return "You Lose! Paper beats Rock!";
        else return"You WIN! Rock beats Scissors";
     }
+    if(playerSelection==="PAPER")
+    {
+       if(computerSelection==="Paper") return "It's a draw!";
+       else if(computerSelection==="Scissors") return "You Lose! Paper beats Rock!";
+       else return"You WIN! Paper beats Rock";
+    }
+    if(playerSelection==="SCISSORS")
+    {
+       if(computerSelection==="Scissors") return "It's a draw!";
+       else if(computerSelection==="Rock") return "You Lose! Rock beats Scissors!";
+       else return"You WIN! Scissors beat Paper";
+    }
 }
 
+
 let playerChoice;
-playerChoice=prompt("Select a weapon!");
-playerChoice=playerChoice.toUpperCase();
+let playerScore=0;
+let computerScore=0;
+
+function game()
+{
+    for(i=1;i<=5;i++)
+    {
+        playerChoice=prompt("Round", i ,"Select a weapon!");
+        playerChoice=playerChoice.toUpperCase();
+        if(playRound(playerChoice,getComputerChoice())==="You WIN! Scissors beat Paper" ||playRound(playerChoice,getComputerChoice())==="You WIN! Paper beats Rock"||playRound(playerChoice,getComputerChoice())==="You WIN! Rock beats Scissors")
+        {
+            playerScore+=1;
+        }
+        else if(playRound(playerChoice,getComputerChoice())==="It's a draw!")
+        {
+            playerScore+=1;
+            computerScore+=1;
+        }
+        else
+        {
+            computerScore+=1;
+        }
+    }
+    alert(playerScore,computerScore);
+}
+
 alert(playRound(playerChoice,getComputerChoice()));
+
