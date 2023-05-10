db0007efb25c83dc5bacd727e6674c763da50988
Foggallon <121718984+Foggallon@users.noreply.github.com>
2023-05-10 14:01:00 +0200

    add function getComputerChoice

--- script.js
+++ script.js
@@ -2,5 +2,18 @@ console.log("Hello Console!");
 
 function getComputerChoice()
 {
-    
+    let computerChoice;
+    computerChoice=Math.floor(Math.random()*100);
+    if(computerChoice>=0 && computerChoice<=33)
+    {
+        return "Rock";
+    }
+    if(computerChoice>=34 && computerChoice<=66)
+    {
+        return "Paper";
+    }
+    if(computerChoice>=67 && computerChoice<=99)
+    {
+        return "Scissors";
+    }
 }
